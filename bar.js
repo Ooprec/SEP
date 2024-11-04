2e1f59656e848ebd2eeef95000ed36d6fdb77302
Sebastian Laza <mr.seb222@gmail.com>
2024-11-04 09:38:12 -0500

    brotha

--- bar.js
+++ bar.js
@@ -3,13 +3,17 @@ src="https://cdn.jsdelivr.net/npm/chart.js"
 
 const ctx = document.getElementById('myChart');
 
+var l = 12
+function but(){
+    l++
+}
 new Chart(ctx, {
   type: 'bar',
   data: {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [l, 19, 3, 5, 2, 3],
       borderWidth: 1
     }]
   },
