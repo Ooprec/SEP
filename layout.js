4bb5d1f31ff081e26d4da82640f4781bdb635bc7
Ooprec <98240045+Ooprec@users.noreply.github.com>
2024-11-05 13:31:56 -0500

    my part

---
+++ layout.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+let inputForm = document.getElementById("uploadForm")
+
+inputForm.addEventListener("change", (e) => {
+    let fileButton = document.getElementById("replacement-button");
+    let upload = document.getElementById("csv");
+
+    if (upload.value == "")
+    {
+        fileButton.innerHTML = "Choose File...";
+    }
+    else {
+        fileButton.innerHTML = upload.value.substring(upload.value.indexOf("h\\")+2);
+    }
+    
+});
+
+inputForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // Don't clear the input when the submit button is clicked
+  
+    let upload = document.getElementById("csv");
+    // console.log(upload);
+    if (upload.value == '')
+    {
+        alert("Make sure you upload a CSV");
+    }
+    else
+    {   
+        console.log(upload.files[0])
+        // csvReader(upload);
+    }});
+
+function csvReader(upload) {
+    let reader = new FileReader();
+
+}
