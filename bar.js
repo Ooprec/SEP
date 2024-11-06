d14a92189f868bd05bee49d5e89195e28c18297a
Sebastian Laza <mr.seb222@gmail.com>
2024-11-06 13:47:40 -0500

    pushy pushy

--- bar.js
+++ bar.js
@@ -1,12 +1,12 @@
   
 src="https://cdn.jsdelivr.net/npm/chart.js"
 
-const ctx = document.getElementById('myChart');
 
 var l = 12
-function but(){
-    l++
-}
+const ctx = document.getElementById('myChart');
+const ctx2 = document.getElementById('myChart2');
+
+
 new Chart(ctx, {
   type: 'bar',
   data: {
@@ -14,6 +14,26 @@ new Chart(ctx, {
     datasets: [{
       label: '# of Votes',
       data: [l, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+document.getElementById('myChart').remove();
+
+new Chart(ctx2, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [l, 3, 7, 5, 6, 10],
       borderWidth: 1
     }]
   },
