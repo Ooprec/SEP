a5b47484d88555447c41a865fa1aeb21cf101704
Ooprec <98240045+Ooprec@users.noreply.github.com>
2024-11-13 21:00:32 -0500

    fixed it all
    yaay

---
+++ vote.js
@@ -0,0 +1,73 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+// TODO: import libraries for Cloud Firestore Database
+// https://firebase.google.com/docs/firestore
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD7vWOt5aS-mmTeGzKwgvcbMrx9K9KxATQ",
+    authDomain: "rank-choice-voting-393ff.firebaseapp.com",
+    projectId: "rank-choice-voting-393ff",
+    storageBucket: "rank-choice-voting-393ff.appspot.com",
+    messagingSenderId: "1070768963220",
+    appId: "1:1070768963220:web:0c5e5968fa52688458fe81"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+export async function loadFromDatabase () {
+    const databaseItems = await getDocs(collection(db, "rank-choice-voting"));
+
+    //Creates a list of objects, linking each voters decision to an object (voter)
+    var allVotes = [];
+    databaseItems.forEach((item) =>{
+        allVotes.push({ first: item.data().first, second: item.data().second, third: item.data().third });
+        
+    });
+
+    sessionStorage.setItem('allVotes', JSON.stringify(allVotes));
+
+}
+
+export async function vote() {
+    
+    var allVotes = JSON.parse(sessionStorage.getItem('allVotes')); 
+    
+
+
+    // for (var voter in allVotes){
+    //     console.log(allVotes[voter].first);
+    // }
+    // console.log(allVotes.length)
+   
+    var candidates = [];
+    var votes = [];
+
+    //makes list of all candidates and their votes
+    for (var i=0; i<allVotes.length; i++){
+        // console.log(allVotes[i].first);
+        if(!candidates.includes(allVotes[i].first)){
+            // console.log(allVotes[i])
+            candidates.push(allVotes[i].first);
+            votes.push(0);
+            // console.log(candidates);
+        }
+    }
+
+     // tallies first round votes
+     for(var i in allVotes){
+        for(var j in candidates){
+            if(allVotes[i].first == candidates[j]){
+            votes[j] = votes[j] + 1;
+         }
+     }
+}
+        console.log(votes);
+        
+}
+//     console.log("candidates");
+// console.log(candidates);
