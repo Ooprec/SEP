4e289dde03aa94153730176040b2ea7779e901d1
Ooprec <98240045+Ooprec@users.noreply.github.com>
2025-01-28 11:01:30 -0500

    made uploading elections a dropdown
    hope this doesn't break it

--- animations.js
+++ animations.js
@@ -53,3 +53,18 @@ LBar.addEventListener("animationend", (e) => {
         LText.innerHTML = "Election complete!"
     }
 })
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
