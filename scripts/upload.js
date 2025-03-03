739b6a1dad41e52a689c3e36c78e36b0739db268
Ooprec <98240045+Ooprec@users.noreply.github.com>
2025-03-03 09:20:56 -0500

    fr

--- scripts/upload.js
+++ scripts/upload.js
@@ -25,8 +25,6 @@ export async function getCollectionList()
   const docsArray = docSnap.data().docsArray;
   const select = document.getElementById("csv-options");
 
-  console.log(docsArray);
-
   select.innerHTML = '';
 
   for (let i = 0; i<docsArray.length; i++) {
@@ -36,122 +34,91 @@ export async function getCollectionList()
   }
 
   return docsArray;
-  
 }
 
-export async function anotherFix() {
-  const listRef = doc(db, "rank-choice-voting", "docList");
-  const docSnap = await getDoc(listRef);
-  const docsArray = docSnap.data().docsArray;
-  const select = document.getElementById("csv-options");
-
-  console.log(docsArray);
-  let updatedArray = docsArray;
-  updatedArray.pop();
-  updatedArray.pop();
-
-  await updateDoc(listRef, {
-    docsArray: updatedArray
-  });
-  select.innerHTML = '';
-
-  
-  }
-
-
-
-   //makes it so you can add csv data to a database
-   //shoutout carson noble for this code
 export async function importCSVToDatabase () {
-    // const studentPieces = await getDocs(collection(db, "rank-choice-voting"));
-    //   studentPieces.forEach((piece) =>{
-    //     deleteDoc(doc(db, "rank-choice-voting", piece.id));
-    //   } );
+  // const studentPieces = await getDocs(collection(db, "rank-choice-voting"));
+  //   studentPieces.forEach((piece) =>{
+  //     deleteDoc(doc(db, "rank-choice-voting", piece.id));
+  //   } );
   console.log("importing")
+  try{   
+    var uname = document.getElementById("csv-name").value;
+            
+    if (uname in getCollectionList()) {alert("Election name already taken"); return}
+    const listRef = doc(db, "rank-choice-voting", "docList");
+    const docSnap = await getDoc(listRef);
+    const docsArray = docSnap.data().docsArray;
+    let updatedDoc = docsArray;
+    updatedDoc.push(uname);
 
-      try{
-            
-            var uname = document.getElementById("csv-name").value;
-            
-            if (uname in getCollectionList()) {alert("Election name already taken"); return}
-
-            const listRef = doc(db, "rank-choice-voting", "docList");
-            const docSnap = await getDoc(listRef);
-            const docsArray = docSnap.data().docsArray;
-            let updatedDoc = docsArray;
-            updatedDoc.push(uname);
-            
-            await updateDoc(listRef, {
-              docsArray: updatedDoc,
-            })
-            console.log("uname updates")
-            var file = document.getElementById("csv").files[0];
-            var reader = new FileReader();
-            reader.onload = function(event) {
-              var csvData = event.target.result;
-              //seperates the csv data into rows to seperate candidates
-              var rows = csvData.split("\n");
-              //looping through all rows, fills rows
-              for (var i = 1; i < rows.length; i++) {
-                //cells is an array of cells
-                var cells = rows[i].split(",");
-               
-                //sets each vote casted by each person equal to a value
-                //represented in fire base as (e.g. First: "Name of Candidate")
-                try {
-                    const docRef = addDoc(collection(db, uname), {
-                    first: cells[2],
-                    second: cells[3],
-                    third: cells[4],
-                    
-                  });
-                  //tests to make sure the code fires
-                  console.log("Document written with ID: ", docRef.id);
+    await updateDoc(listRef, {
+      docsArray: updatedDoc,
+    })
+    console.log("uname updates")
+    var file = document.getElementById("csv").files[0];
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      var csvData = event.target.result;
+      //seperates the csv data into rows to seperate candidates
+      var rows = csvData.split("\n");
+      //looping through all rows, fills rows
+      for (var i = 1; i < rows.length; i++) {
+        //cells is an array of cells
+        var cells = rows[i].split(",");   
+        //sets each vote casted by each person equal to a value
+        //represented in fire base as (e.g. First: "Name of Candidate")
+        try {
+            const docRef = addDoc(collection(db, uname), {
+              first: cells[2],
+              second: cells[3],
+              third: cells[4],      
+            });
+            //tests to make sure the code fires
+            console.log("Document written with ID: ", docRef.id); 
+            // console.log("Document written with ID: ", docRef.id);
+        } 
                 
-                  // console.log("Document written with ID: ", docRef.id);
-                } 
-              
-                catch (e) {
-                  console.error("Error adding votes to database: ", e);
-                  
-                }
-                
-              }
-            };
-            reader.readAsText(file);
-            file.innerHTML = null; 
-            location.reload();    
-    }
-    //catch commander to prevent system stoppage in the event of wrongly submitted info
-    catch (e) 
-    {
-        console.error("Error adding votes to database: ", e);   
-    }
+        catch (e) {
+          console.error("Error adding votes to database: ", e);     
+        }  
+      }
+    };
+    reader.readAsText(file);
+    file.innerHTML = null; 
+       
+  }
+  //catch commander to prevent system stoppage in the event of wrongly submitted info
+  catch (e) 
+  {
+    console.error("Error adding votes to database: ", e);   
+  }
+  location.reload(); 
 
 }
 
-export const deleteProject = async function(){
-  const projName = document.getElementById("csv-options").value;
+export const destroyVotes = async function(){
+  const election = document.getElementById("csv-options").value;
   const select = document.getElementById("csv-options");
   const options = select.getElementsByTagName('option');
   select.dispatchEvent(new Event('change'));
   for (let i = 0; i < options.length; i++) {
-    if (options[i].value === projName) {
+    if (options[i].value === election) {
       select.removeChild(options[i]);
       break;
     }
   }
   select.dispatchEvent(new Event('change'));
-  const allDocumentsInCollection = await getDocs(collection(db, projName));
+  const allDocumentsInCollection = await getDocs(collection(db, election));
   allDocumentsInCollection.forEach(item => {
-    deleteDoc(doc(db, projName, item.id));
+    deleteDoc(doc(db, election, item.id));
   });
   const listRef = doc(db, "rank-choice-voting", "docList");
   const docSnap = await getDoc(listRef);
 
 
   let updatedArray = docSnap.data().docsArray;
-  let index = updatedArray.indexOf(projName);
+  let index = updatedArray.indexOf(election);
   updatedArray.splice(index, 1);
   console.log(docSnap.data().docsArray + "___" + updatedArray);
 
@@ -167,7 +134,6 @@ submit.addEventListener("click", (e)=> {
   e.preventDefault();
   importCSVToDatabase();
   getCollectionList();
-  // document.getElementById("csv-name").value = "";
-  // document.getElementById("csv").value = "";
+
 
 })
